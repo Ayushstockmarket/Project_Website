58298521cd045d0f3e98436a738975b23a054e2f
Ayushstockmarket <161935651+Ayushstockmarket@users.noreply.github.com>
2025-01-14 09:07:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+function ScrollTopBottom(){document.getElementById("scrollTop").addEventListener("click",function(){window.scrollTo({top:0,behavior:"smooth",});});document.getElementById("scrollBottom").addEventListener("click",function(){window.scrollTo({top:document.body.scrollHeight,behavior:"smooth",});});}
+window.addEventListener("load",()=>{ScrollTopBottom();});
